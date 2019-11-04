d4d4a214a32148d199b3f5d1cde8f6be6b79ad80
Wally <wjz1095@gmail.com>
2019-11-04 15:50:29 -0500

    Integers not being declared properly
    The integers are not able to be declared and then accessed by another Sub in the form. Need to figure out how to declare them so other Subs can access them as I do not want the declaration of variables to be within the button click events

--- CustomCounter/CustomCounter/MainForm.vb
+++ CustomCounter/CustomCounter/MainForm.vb
@@ -28,7 +28,26 @@
         Counter10Label.Text = "0"
         Counter12Label.Text = "0"
 
+        'Create counter variables and set all to 0
+        Dim Counter1Count As Integer = 0
+        Dim Counter2Count As Integer = 0
+        Dim Counter3Count As Integer = 0
+        Dim Counter4Count As Integer = 0
+        Dim Counter5Count As Integer = 0
+        Dim Counter6Count As Integer = 0
+        Dim Counter7Count As Integer = 0
+        Dim Counter8Count As Integer = 0
+        Dim Counter9Count As Integer = 0
+        Dim Counter10Count As Integer = 0
+        Dim Counter11Count As Integer = 0
+        Dim Counter12Count As Integer = 0
     End Sub
+
+    Public Module DefineVariables
+        Public Counter1Count As Integer = 0
+
+    End Module
+
 
     Private Sub Counter1ResetButton_Click(sender As Object, e As EventArgs) Handles Counter1ResetButton.Click
         Counter1TextBox.Text = ""
@@ -88,5 +107,103 @@
     Private Sub Counter12ResetButton_Click(sender As Object, e As EventArgs) Handles Counter12ResetButton.Click
         Counter12TextBox.Text = ""
         Counter12Label.Text = "0"
+    End Sub
+
+    Public Sub Counter1MinusButton_Click(sender As Object, e As EventArgs) Handles Counter1MinusButton.Click
+        Counter1Count = Counter1Count - 1
+
+    End Sub
+
+    Private Sub Counter1PlusButton_Click(sender As Object, e As EventArgs) Handles Counter1PlusButton.Click
+
+    End Sub
+
+    Private Sub Counter2MinusButton_Click(sender As Object, e As EventArgs) Handles Counter2MinusButton.Click
+
+    End Sub
+
+    Private Sub Counter2PlusButton_Click(sender As Object, e As EventArgs) Handles Counter2PlusButton.Click
+
+    End Sub
+
+    Private Sub Counter3MinusButton_Click(sender As Object, e As EventArgs) Handles Counter3MinusButton.Click
+
+    End Sub
+
+    Private Sub Counter3PlusButton_Click(sender As Object, e As EventArgs) Handles Counter3PlusButton.Click
+
+    End Sub
+
+    Private Sub Counter4MinusButton_Click(sender As Object, e As EventArgs) Handles Counter4MinusButton.Click
+
+    End Sub
+
+    Private Sub Counter4PlusButton_Click(sender As Object, e As EventArgs) Handles Counter4PlusButton.Click
+
+    End Sub
+
+    Private Sub Counter5MinusButton_Click(sender As Object, e As EventArgs) Handles Counter5MinusButton.Click
+
+    End Sub
+
+    Private Sub Counter5PlusButton_Click(sender As Object, e As EventArgs) Handles Counter5PlusButton.Click
+
+    End Sub
+
+    Private Sub Counter6MinusButton_Click(sender As Object, e As EventArgs) Handles Counter6MinusButton.Click
+
+    End Sub
+
+    Private Sub Counter6PlusButton_Click(sender As Object, e As EventArgs) Handles Counter6PlusButton.Click
+
+    End Sub
+
+    Private Sub Counter7MinusButton_Click(sender As Object, e As EventArgs) Handles Counter7MinusButton.Click
+
+    End Sub
+
+    Private Sub Counter7PlusButton_Click(sender As Object, e As EventArgs) Handles Counter7PlusButton.Click
+
+    End Sub
+
+
+    Private Sub Counter8MinusButton_Click(sender As Object, e As EventArgs) Handles Counter8MinusButton.Click
+
+    End Sub
+
+    Private Sub Counter8PlusButton_Click(sender As Object, e As EventArgs) Handles Counter8PlusButton.Click
+
+    End Sub
+
+    Private Sub Counter9MinusButton_Click(sender As Object, e As EventArgs) Handles Counter9MinusButton.Click
+
+    End Sub
+
+    Private Sub Counter9PlusButton_Click(sender As Object, e As EventArgs) Handles Counter9PlusButton.Click
+
+    End Sub
+
+    Private Sub Counter10MinusButton_Click(sender As Object, e As EventArgs) Handles Counter10MinusButton.Click
+
+    End Sub
+
+    Private Sub Counter10PlusButton_Click(sender As Object, e As EventArgs) Handles Counter10PlusButton.Click
+
+    End Sub
+
+    Private Sub Counter11MinusButton_Click(sender As Object, e As EventArgs) Handles Counter11MinusButton.Click
+
+    End Sub
+
+    Private Sub Counter11PlusButton_Click(sender As Object, e As EventArgs) Handles Counter11PlusButton.Click
+
+    End Sub
+
+    Private Sub Counter12MinusButton_Click(sender As Object, e As EventArgs) Handles Counter12MinusButton.Click
+
+    End Sub
+
+    Private Sub Counter12PlusButton_Click(sender As Object, e As EventArgs) Handles Counter12PlusButton.Click
+
     End Sub
 End Class
